7338635662fb4c70268e085e2a34eeab42df69e5
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-01-10 10:39:18 +0100

    Add mockgen

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 * the goal of this project is to learn SQLC, implement migrations using Docker, and set up GitHub Actions
 * the approach will be guided by Test-Driven Development (TDD)
+
+### Mock gen
+* mockgen -destination db/mock/store.go github.com/redmonkez12/go-project-2/db/sqlc Store
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,19 +6,24 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type Store struct {
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+type SQLStore struct {
 	*Queries
 	db *pgxpool.Pool
 }
 
-func NewStore(db *pgxpool.Pool) *Store {
-	return &Store{
+func NewStore(db *pgxpool.Pool) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
-func (store *Store) ExecTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) ExecTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.Begin(ctx)
 	if err != nil {
 		return err
@@ -48,7 +53,7 @@ type TransferTxResult struct {
 	ToEntry     Entry    `json:"to_entry"`
 }
 
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.ExecTx(ctx, func(q *Queries) error {
